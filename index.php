5c8a11aa1e7cff3f03bc51d14590972043a484b7
SteveBotella <steve.botella@le-campus-numerique.fr>
2021-01-25 16:28:43 +0100

    blog v1.2

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-hello
+<?php
+$raw_url = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
+if (isset($raw_url)) {
+    switch ($raw_url) {
+        case 'home':
+            include 'index.php';
+            break;
+        default:
+            include 'index.php';//TODO 404
+    }
+} else {
+    include 'index.php';
+}
