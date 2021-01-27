9161d7d323128283b29676014a76e9759fbce440
SteveBotella <steve.botella@le-campus-numerique.fr>
2021-01-27 13:25:37 +0100

    blog v1.5

--- app/persistences/blogPostData.php
+++ app/persistences/blogPostData.php
@@ -1,10 +1,13 @@
 <?php
 //retourne les 10 derniers posts avec leur auteurs.
-function lastBlogPosts (PDO $dbh) {
-    return $dbh->query('SELECT postTitle, authors.name
+function lastBlogPosts($dbh){
+$statementhandle = $dbh->prepare('SELECT postTitle, authors.name
 FROM `posts`
 INNER JOIN authors
 ON authors_id=authors.id
 ORDER BY `publicationStart` DESC
 LIMIT 10');
+$statementhandle->execute();
+$result = $statementhandle->fetchAll(\PDO::FETCH_ASSOC);
+return $result;
 }
