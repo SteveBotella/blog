45638ec1243b353eb25cd32eb87d268d5e98f789
SteveBotella <steve.botella@le-campus-numerique.fr>
2021-01-28 11:54:22 +0100

    Update home.tpl with foreach

--- ressources/views/home.tpl.php
+++ ressources/views/home.tpl.php
@@ -1,8 +1,5 @@
-<?php
-foreach ($resultTableArticle as $value){
-    echo '<p>'. $value['postTitle'], $value['nickname']. '</p>';
-}
-
-if (!empty($resultTableArticle)) {
-    foreach ($resultTableArticle as $value)
-}
+<?php if (!empty($resultTableArticle)): ?>
+<?php foreach ($resultTableArticle as $value): ?>
+<div><?=$value['postTitle']?> <?=$value['nickname']?></div>
+<?php endforeach; ?>
+<?php endif; ?>
